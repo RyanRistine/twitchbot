417edd7893c7377ab946bff654a7e6159090b990
Ryan Ristine <ryan@Hal.attlocal.net>
2016-10-09 08:05:57 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var tmi = require ('tmi.js');
+
+var options = {
+    options: {
+      debug: true
+    },
+    connection: {
+      cluster: "aws",
+      reconnect: true
+    },
+    identity: {
+      username: "banwipzbot",
+      password: "oauth:3mxorxxaxyrp4xnt61woo2gnga5qh5"
+    },
+    channels: ["supermansbrother"]
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on('join', function (channel, username, self) {
+  if(username = "WipzTV") {
+    client.ban ("supermansbrother", "WipzTV", "Wipz in 2016 LUL")
+  } else {
+      client.action("supermansbrother", user['display-name'] + ", welcome to the stream!")
+  }
+});
+
+client.on('connected', function(address, port) {
+  client.action("supermansbrother", "Hello I am BanWipzBot, my sole purpose in life is to ban WipzTV");
+});
