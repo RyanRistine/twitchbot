b769b79b25d0d65e987b56c1cba07a612bbcdcc8
Ryan Ristine <ryanristine01@gmail.com>
2016-12-23 14:44:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var client = new tmi.client(options);
 client.connect();
 
 client.on('join', function (channel, username, self) {
-    if(channel === "supermansbrother" && username === "WipzTV"){
-      client.ban("supermansbrother", "WipzTV", "Wipz in 2016 LUL")
+    if(channel === "supermansbrother" && username === "wipztv"){
+      client.ban("supermansbrother", "wipztv", "Wipz in 2016 LUL")
     }
 });
 
