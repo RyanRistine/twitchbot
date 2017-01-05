2ca4c158410e59a1f404dd81f834d7a487dafad6
Ryan Ristine <ryanristine01@gmail.com>
2017-01-05 10:56:18 -0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var options = {
       username: "your-bots-username-here",
       password: "oauthtokenhere"
     },
-    channels: ["your-cahnnel-here"]
+    channels: ["your-channel-here"]
 };
 
 var client = new tmi.client(options);
@@ -20,10 +20,10 @@ client.connect();
 
 client.on('join', function (channel, username, self) {
     if(channel === "your-channel-here" && username === "wipztv"){
-      client.ban("your-channel-here", "wipztv", "Wipz in 2016 LUL")
+      client.ban("your-channel-here", "user-of-person-you-want-to-ban", "'user-of-person-you-want-to-ban' in 2016 LUL")
     }
 });
 
 client.on('connected', function(address, port) {
-  client.action("your-channel-here", "Hello I am BanWipzBot, my sole purpose in life is to ban WipzTV");
+  client.action("your-channel-here", "Hello I am 'your-bots-username-here', my sole purpose in life is to ban 'user-of-person-you-want-to-ban");
 });
